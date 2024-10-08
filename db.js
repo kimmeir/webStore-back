07470f66980725e12776d32e276d59eb45f7355a
kimmeir <kimmmeir@gmail.com>
2024-10-08 14:28:15 +0300

    added .env

--- db.js
+++ db.js
@@ -1,15 +1,18 @@
 const { Sequelize } = require('sequelize')
+const env = require('dotenv').config()
 
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env
 
 const sequelize = new Sequelize({
-  database: DB_NAME || 'webstore',
-  username: DB_USER || 'postgres',
-  password: DB_PASSWORD || 'root',
-  host: DB_HOST || 'localhost',
-  port: DB_PORT || 5432,
+  database: DB_NAME,
+  username: DB_USER,
+  password: DB_PASSWORD,
+  host: DB_HOST,
+  port: DB_PORT,
   dialect: 'postgres',
 })
 
 module.exports = sequelize
+
+
 
